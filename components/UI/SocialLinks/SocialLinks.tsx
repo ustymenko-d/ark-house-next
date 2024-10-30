9acf398f4873cb99eca239555b4ba22fbe244afe
@andry125177 <andry125177@gmail.com>
2024-10-30 09:27:32 +0200

    Remove sass and src folder

--- components/UI/SocialLinks/SocialLinks.tsx
+++ components/UI/SocialLinks/SocialLinks.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import styles from './SocialLinks.module.scss'
+import styles from './SocialLinks.module.css'
 
 const SocialLinks: React.FC = () => {
 	const BASIC_CLASSES: string =
