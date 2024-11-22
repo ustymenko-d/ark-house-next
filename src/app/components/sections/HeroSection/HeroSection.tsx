b1964b45a61c0cf4cc5ad2d28868594095626301
ustymenko-d <ustymenko.denys@gmail.com>
2024-11-22 09:23:32 +0200

    Hero section slogan fix

--- src/app/components/sections/HeroSection/HeroSection.tsx
+++ src/app/components/sections/HeroSection/HeroSection.tsx
@@ -14,8 +14,8 @@ const HeroSection: React.FC = () => {
 			className={`${styles.hero} container mx-auto md:px-10 lg:px-20 relative overflow-hidden py-16 mb-10 flex flex-col items-start gap-12 justify-center md:justify-normal md:bg-none lg:gap-0 lg:flex-row lg:justify-between `}>
 			<h1
 				className={`${styles.title} z-10 relative mb-10 px-4 pt-4 font-bold md:text-8xl lg:px-0`}>
-				<span className='absolute top-0 right-3 font-medium text-lg md:tracking-wide'>
-					Since&nbsp;1975&nbsp;—
+				<span className='absolute top-0 right-3 mr-4 font-medium text-lg tracking-wide'>
+					Since&nbsp;1975&nbsp;-
 				</span>
 				Architecture.
 				<br />
