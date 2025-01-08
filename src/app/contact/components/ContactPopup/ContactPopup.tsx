067dafbe4efdb6d55e24d1184e33cd8a3889feb3
ustymenko-d <ustymenko.denys@gmail.com>
2025-01-08 02:49:04 +0200

    Change div to output in ContactPopup

--- src/app/contact/components/ContactPopup/ContactPopup.tsx
+++ src/app/contact/components/ContactPopup/ContactPopup.tsx
@@ -8,7 +8,7 @@ interface ContactPopupProps {
 const ContactPopup: React.FC<ContactPopupProps> = ({ togglePopupShow }) => {
 	return (
 		<div className='fixed top-4 left-1/2 -translate-x-1/2 z-50 mx-auto px-4 max-lg:container pointer-events-none'>
-			<div className='mx-auto p-4 bg-white shadow-2xl flex gap-4 items-start justify-between max-w-fit pointer-events-auto'>
+			<output className='mx-auto p-4 bg-white shadow-2xl flex gap-4 items-start justify-between max-w-fit pointer-events-auto'>
 				<p className='lg:w-4/5'>
 					<span className='font-bold text-lg'>Successfully submitted.</span>
 					<br /> <strong>Note:</strong> This is an example page, and no data has
@@ -18,7 +18,7 @@ const ContactPopup: React.FC<ContactPopupProps> = ({ togglePopupShow }) => {
 					className={`${styles.button} relative w-8 h-8 aspect-square shadow`}
 					onClick={() => togglePopupShow(false)}
 					aria-label='Close popup'></button>
-			</div>
+			</output>
 		</div>
 	)
 }
