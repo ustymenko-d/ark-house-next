237844f0635020bbb8c24fa7423f6d7bbbf6cb08
ustymenko-d <ustymenko.denys@gmail.com>
2025-08-15 22:39:40 +0300

    fix: hero bg animation and dvh height change on mobile

--- src/app/components/sections/Hero/Hero.tsx
+++ src/app/components/sections/Hero/Hero.tsx
@@ -1,3 +1,5 @@
+import clsx from 'clsx'
+
 import AnimatedWrapper from '@/components/AnimatedWrapper'
 import Button from '@/components/UI/Button'
 import SocialLinks from '@/components/UI/SocialLinks/SocialLinks'
@@ -6,19 +8,22 @@ import styles from './Hero.module.css'
 
 const Hero = () => (
 	<div
-		className={`${styles.hero} container mx-auto md:px-10 lg:px-20 relative overflow-hidden py-16 mb-10 flex flex-col items-start gap-12 justify-center md:justify-normal md:bg-none lg:gap-0 lg:flex-row lg:justify-between`}>
+		className={clsx(
+			styles.hero,
+			'container mx-auto md:px-10 lg:px-20 relative overflow-hidden py-16 mb-10 flex flex-col items-start gap-12 justify-center md:justify-normal md:bg-none lg:gap-0 lg:flex-row lg:justify-between'
+		)}>
 		<div className='text-[clamp(2.625rem,1.0179rem+8.0357vw,4.875rem)] z-10 relative mb-10 px-4 pt-4 font-bold md:text-8xl lg:px-0'>
 			<span className='absolute top-0 mr-4 text-lg font-medium tracking-wide right-3'>
 				Since&nbsp;1975&nbsp;-
 			</span>
 			<AnimatedWrapper className='max-h-fit'>Architecture.</AnimatedWrapper>
 			<AnimatedWrapper
-				delay={0.3}
+				delay={0.2}
 				className='max-h-fit'>
 				Planning.
 			</AnimatedWrapper>
 			<AnimatedWrapper
-				delay={0.5}
+				delay={0.4}
 				className='max-h-fit'>
 				Design.
 			</AnimatedWrapper>
@@ -28,8 +33,16 @@ const Hero = () => (
 			<SocialLinks />
 		</div>
 
-		<div
-			className={`${styles.background} absolute left-0 right-0 bottom-0 flex flex-col justify-end items-center overflow-hidden pointer-events-none bg-no-repeat`}></div>
+		<AnimatedWrapper
+			delay={0.2}
+			duration={0.6}
+			className='absolute bottom-0 left-0 right-0 w-full h-full'>
+			<div
+				className={clsx(
+					styles.background,
+					'absolute left-0 right-0 bottom-0 flex flex-col justify-end items-center overflow-hidden pointer-events-none bg-no-repeat'
+				)}></div>
+		</AnimatedWrapper>
 	</div>
 )
 
