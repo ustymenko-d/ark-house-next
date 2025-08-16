fd13a9892ddaba3da30533983da6475945dd1e5a
ustymenko-d <ustymenko.denys@gmail.com>
2025-08-16 15:45:13 +0300

    style: history page

--- src/app/history/components/sections/About.tsx
+++ src/app/history/components/sections/About.tsx
@@ -3,13 +3,13 @@ import Button from '@/components/UI/Button'
 import ImageWithSkeleton from '@/components/UI/ImageWithSkeleton'
 
 const About = () => (
-	<section className='mb-16 xl:grid xl:gap-x-6 xl:grid-cols-[61.3%_auto] xl:grid-rows-[auto_1fr]'>
-		<h2 className='mb-4 text-5xl font-semibold leading-normal tracking-tight xl:col-span-2'>
+	<section className='mb-14 grid gap-6 xl:grid-cols-[61.3%_auto] xl:grid-rows-[auto_1fr]'>
+		<h1 className='text-4xl font-semibold leading-tight tracking-tight md:text-5xl xl:col-span-2'>
 			Who we are
-		</h2>
+		</h1>
 
 		<ImageWithSkeleton
-			className='w-full h-full mb-4 bg-dark-color xl:mb-0'
+			className='w-full h-full bg-dark-color'
 			src='/img/history-page/meet.jpg'
 			width={844}
 			height={563}
@@ -18,24 +18,28 @@ const About = () => (
 			alt='Meet'
 		/>
 
-		<div className='text-xl'>
-			<p className='mb-6'>
+		<div className='flex flex-col gap-4 text-base md:text-lg'>
+			<p className='text-gray-800 text-balance'>
 				We are dedicated individuals from a range of different countries. Based in Oslo, with
 				offices in Copenhagen, London and Reykjavik, we are actively involved in projects in various
 				countries across Europe, Africa, the Middle East and Asia.
 			</p>
-			<p className='mb-6'>
+			<p className='text-gray-800 text-balance'>
 				Together, we are a community with a culture for beautiful and outstanding architecture, a
 				culture that is Powered by Commitment.
 			</p>
-			<AnimatedWrapper>
-				<p className='mb-4 text-3xl font-semibold leading-tight tracking-tight sm:text-4xl'>
-					Do you want to work with us?
-				</p>
-			</AnimatedWrapper>
-			<AnimatedWrapper delay={0.2}>
-				<Button to='/contact'>Our&nbsp;Vacancies</Button>
-			</AnimatedWrapper>
+			<div className='flex flex-col gap-6 pt-4 xl:mt-auto'>
+				<AnimatedWrapper>
+					<p className='text-2xl font-semibold leading-tight tracking-tight'>
+						Do you want to work with us?
+					</p>
+				</AnimatedWrapper>
+				<AnimatedWrapper
+					delay={0.2}
+					className='flex'>
+					<Button to='/contact'>Our&nbsp;Vacancies</Button>
+				</AnimatedWrapper>
+			</div>
 		</div>
 	</section>
 )
--- src/components/Breadcrumbs.tsx
+++ src/components/Breadcrumbs.tsx
@@ -8,26 +8,23 @@ interface INavItem {
 
 const Breadcrumbs = ({ navItems }: { navItems: INavItem[] }) => (
 	<nav
-		className='pt-12 mb-8'
+		className='pt-10 md:pt-12 mb-6 md:mb-8'
 		aria-label='Breadcrumb'>
-		<ol className='flex flex-wrap items-center text-xl list-none gap-x-4 gap-y-2'>
+		<ol className='flex flex-wrap items-center gap-x-4 md:gap-x-6 gap-y-2 text-base md:text-lg leading-relaxed list-none'>
 			{navItems.map((item, index) =>
 				index === navItems.length - 1 ? (
 					<li
 						key={`${item.name}-${index}`}
-						className='font-semibold'>
-						<span
-							key={`${item.name}-${index}`}
-							aria-current='page'>
-							{item.name}
-						</span>
+						className='font-semibold text-gray-900'
+						aria-current='page'>
+						{item.name}
 					</li>
 				) : (
 					<li
 						key={`${item.name}-${index}`}
 						className={clsx(
-							'relative leading-tight mr-10',
-							'after:absolute after:top-1/2 after:translate-y-[-50%] after:right-[-40px] after:content-[url("/img/icons/arrow.svg")] after:leading-none after:pointer-events-none'
+							'relative leading-tight mr-8',
+							'after:absolute after:top-1/2 after:translate-y-[-50%] after:right-[-40px] after:content-[url("/img/icons/arrow.svg")] after:leading-[0] after:pointer-events-none'
 						)}>
 						<Link
 							className='animatedUnderline text-dark-color'
