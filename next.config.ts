5f2484a5631c2ffb60b47f7f36287ed7b17f4c24
ustymenko-d <ustymenko.denys@gmail.com>
2025-08-17 13:51:49 +0300

    perf: home page hero image lcp optimization

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,10 @@
 import type { NextConfig } from 'next'
 
-const nextConfig: NextConfig = {}
+const nextConfig: NextConfig = {
+	images: {
+		deviceSizes: [640, 768, 1024, 1280, 1536, 1920, 2048, 3840],
+		formats: ['image/avif', 'image/webp'],
+	},
+}
 
 export default nextConfig
--- src/app/components/sections/Hero/Hero.tsx
+++ src/app/components/sections/Hero/Hero.tsx
@@ -4,15 +4,23 @@ import AnimatedWrapper from '@/components/AnimatedWrapper'
 import Button from '@/components/UI/Button'
 import SocialLinks from '@/components/UI/SocialLinks/SocialLinks'
 
-import styles from './Hero.module.css'
+import HeroImage from './components/HeroImage'
 
 const Hero = () => (
 	<div
 		className={clsx(
-			styles.hero,
-			'container mx-auto md:px-10 lg:px-20 relative overflow-hidden py-16 mb-10 flex flex-col items-start gap-12 justify-center md:justify-normal md:bg-none lg:gap-0 lg:flex-row lg:justify-between'
+			'container relative mx-auto min-h-[calc(100dvh-78px)] max-h-[800px] py-16 mb-10 flex flex-col items-start gap-12 justify-center overflow-hidden',
+			'md:px-10 md:justify-normal md:bg-none',
+			'lg:min-h-[800px] lg:max-h-[1000px] lg:h-[calc(100dvh-93px)] lg:px-20 lg:flex-row lg:gap-0 lg:justify-between',
+			'duration-500',
+			'before:content-[""] before:absolute before:left-0 before:right-0 before:bottom-0 before:w-full before:h-[70%] before:bg-[#f8f8f8] before:z-0',
+			'lg:before:w-[calc(100%-160px)] lg:before:min-h-[63%] lg:before:mx-auto'
 		)}>
-		<div className='text-[clamp(2.625rem,1.0179rem+8.0357vw,4.875rem)] z-10 relative mb-10 px-4 pt-4 font-bold md:text-8xl lg:px-0'>
+		<div
+			className={clsx(
+				'relative z-10 mb-10 px-4 pt-4 font-bold text-[clamp(2.625rem,1.0179rem+8.0357vw,4.875rem)]',
+				'md:text-8xl lg:px-0'
+			)}>
 			<span className='absolute top-0 mr-4 text-lg font-medium tracking-wide right-3'>
 				Since&nbsp;1975&nbsp;-
 			</span>
@@ -28,21 +36,17 @@ const Hero = () => (
 				Design.
 			</AnimatedWrapper>
 		</div>
-		<div className='z-10 flex flex-col items-start gap-8 px-4 lg:items-end lg:px-0 lg:self-center xl:self-start xl:gap-20 xl:items-end'>
+
+		<div
+			className={clsx(
+				'z-10 flex flex-col items-start gap-8 px-4 lg:items-end lg:px-0 lg:self-center',
+				'xl:self-start xl:gap-20 xl:items-end'
+			)}>
 			<Button to='/contact'>Contact&nbsp;us</Button>
 			<SocialLinks />
 		</div>
 
-		<AnimatedWrapper
-			delay={0.2}
-			duration={0.6}
-			className='absolute bottom-0 left-0 right-0 w-full h-full'>
-			<div
-				className={clsx(
-					styles.background,
-					'absolute left-0 right-0 bottom-0 flex flex-col justify-end items-center overflow-hidden pointer-events-none bg-no-repeat'
-				)}></div>
-		</AnimatedWrapper>
+		<HeroImage />
 	</div>
 )
 
